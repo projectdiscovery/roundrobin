f3f5553da3530a859734368403287752295d5fd5
mzack <marco.rivoli.nvh@gmail.com>
2021-10-19 11:42:17 +0200

    adding upstream request number

--- transport/transport.go
+++ transport/transport.go
@@ -16,6 +16,16 @@ func New(proxies ...string) (*RoundTransport, error) {
 	return &RoundTransport{rb: rb}, nil
 }
 
+func NewWithOptions(upstreamRequestNumber int, proxies ...string) (*RoundTransport, error) {
+	rbOptions := roundrobin.DefaultOptions
+	rbOptions.RotateAmount = int32(upstreamRequestNumber)
+	rb, err := roundrobin.NewWithOptions(rbOptions, proxies...)
+	if err != nil {
+		return nil, err
+	}
+	return &RoundTransport{rb: rb}, nil
+}
+
 func (rt *RoundTransport) Next() string {
 	next := rt.rb.Next()
 	return next.String()
